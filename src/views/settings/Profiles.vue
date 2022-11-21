c3e21345c91c3bc304e3ca807dc8297250d8550c
quiquelhappy <quiquelhappy@gmail.com>
2022-11-21 23:15:53 +0100

    settings

---
+++ src/views/settings/Profiles.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-alert color="primary" class="mb-2">
+        You don't have any linked profiles. Link them and instantly verify your resources, get analytics, enable
+        donations, and enable payouts!
+    </v-alert>
+    <v-card class="pa-3 mb-2">
+        <v-list-item title="www.spigotmc.org">
+            <template v-slot:append>
+                <v-btn>
+                    Login using the CLI
+                </v-btn>
+            </template>
+        </v-list-item>
+    </v-card>
+    <v-card class="pa-3 mb-2">
+        <v-list-item title="www.curseforge.com">
+            <template v-slot:append>
+                <v-btn>
+                    Login now
+                </v-btn>
+            </template>
+        </v-list-item>
+    </v-card>
+    <v-card class="pa-3 mb-2">
+        <v-list-item title="www.modrinth.com">
+            <template v-slot:append>
+                <v-btn>
+                    Login now
+                </v-btn>
+            </template>
+        </v-list-item>
+    </v-card>
+</template>
