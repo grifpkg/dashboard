e632c017ae5b2b4f85d23367ff817287988bf73d
quiquelhappy <quiquelhappy@gmail.com>
2022-11-21 09:40:22 +0100

    moved navigation to component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar :model-value="$router.currentRoute.value.path != '/'">
-      <v-container>
-        <router-link to="/" style="text-decoration:none">
-          <v-app-bar-title>
-            grifpkg
-          </v-app-bar-title>
-        </router-link>
-      </v-container>
-    </v-app-bar>
+    <navigation v-if="$router.currentRoute.value.path != '/'" />
     <v-main>
       <router-view />
     </v-main>
@@ -16,19 +8,27 @@
       <v-container class="text-center">
         <small>
           <p>
-            grifpkg is <u>not</u> affiliated to spigotmc.org, curseforge.com or modrinth.com, and complies with
-            their TOS or have negotiated with them in order to deliver resource files. All data from resources
-            hosted
-            by
-            external services are &copy; of their respective authors and platforms.
+            grifpkg is <u>not</u> affiliated to spigotmc.org, curseforge.com or
+            modrinth.com, and complies with their TOS or have negotiated with
+            them in order to deliver resource files. All data from resources
+            hosted by external services are &copy; of their respective authors
+            and platforms.
           </p>
           <p class="mt-5">
-            &copy; grifpkg {{ new Date().getFullYear() }}, limited to our back-end, front-end, CLI and user
-            generated
-            data from grifpkg.com
+            &copy; grifpkg {{ new Date().getFullYear() }}, limited to our
+            back-end, front-end, CLI and user generated data from grifpkg.com
           </p>
         </small>
       </v-container>
     </v-footer>
   </v-app>
 </template>
+
+<script>
+import Navigation from "./components/Navigation.vue";
+export default {
+  components: {
+    Navigation,
+  },
+};
+</script>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app-bar>
+    <v-spacer />
+    <v-col class="text-right">
+      <router-link to="/" style="text-decoration: none">
+        <v-app-bar-title style="font-weight: 900"> grifpkg </v-app-bar-title>
+      </router-link>
+    </v-col>
+    <v-container class="flex-shrink-1">
+      <search-bar />
+    </v-container>
+    <v-col>
+      <v-avatar> 🕵️ </v-avatar>
+    </v-col>
+    <v-spacer />
+  </v-app-bar>
+</template>
+
+<script>
+import SearchBar from './SearchBar.vue';
+export default {
+  components: {
+    SearchBar,
+  },
+};
+</script>
