a7a7d993a6e4819319d4f6ade6d3958ad85b458d
quiquelhappy <quiquelhappy@gmail.com>
2022-11-21 23:15:49 +0100

    legal: cookies, privacy and TOS

---
+++ src/views/legal/TOS.vue
@@ -0,0 +1,244 @@
+<template>
+    <v-sheet class="pa-10 tall-line mb-10">
+        <p>This website (grifpkg.com) is operated by Nominal, throughout the site, the terms "we","us",
+            and "our" refer to Nominal. Nominal offers this website, including all information, tools, and services
+            available from this site to you, the user, conditioned upon your acceptance of all terms, conditions,
+            policies, and notices stated here. </p>
+        <p>By using our site and/ or purchasing something from us, you engage in our “Service” and agree to be bound
+            by the following terms and conditions (“Terms of Service”, “Terms”), including those additional terms
+            and conditions and policies referenced herein and/or available by hyperlink. These Terms of Service
+            apply to all users of the site, including without limitation users who are browsers, vendors, customers,
+            merchants, and/ or contributors of content, regardless of the plan that you have (referred to within
+            these Terms as "you", "your", "client").</p>
+        <p>Before you sign up for our service, you are required to agree to these Terms Of service, if you do not
+            agree, you will not be able to use our service. Please make sure to read the Terms Of Service carefully
+            before creating an account. We may amend these Terms Of Service when we see fit, with no prior
+            notification, it is your responsibility to check this page for changes.</p>
+        <p>The extent of the service that will be provided to you will depend on your current plan, you can find out
+            more about our at our website, located at grifpkg.com.</p>
+        <p>These terms are only available in English and can be viewed from https://grifpkg.com/legal/terms
+        </p>
+        <h2>Section 1 - Information about our service</h2>
+        <ol data-type="ordered">
+            <li>Both the grifpkg website and Nominal website are operated by us.</li>
+            <li>We can be contacted through e-mail at help@grifpkg.com
+                directly, or by using our ticket system, provided at https://grifpkg.com.</li>
+        </ol>
+        <h2>Section 2 - Access to our services</h2>
+        <ol data-type="ordered">
+            <li>When you sign up for our paid-for service, you will gain access to the functionality provided on the
+                plan's page on our website.</li>
+            <li>Your plan will go in effect from the moment that we receive payment, and when you receive
+                confirmation that your plan has been activated.</li>
+            <li>Any plan will have a lifespan of 30 days (1 month).</li>
+            <li>It is your responsibility to renew (extend) your plan before it expires.</li>
+            <li>We may refuse to accept any order by you to purchase a plan at any time at our discretion.</li>
+            <li>We may suspend or cancel your plan at our discretion, we will refund in a pro-active way (we will
+                pay back the days that you have left on your plan, but were not able to use) when the cancellation
+                happens before your plan expires. Except when the cancellation or suspension happened because of a
+                breach of our Terms, in that case, no refund will be issued.</li>
+        </ol>
+        <h2>Section 3 - Fees for our services</h2>
+        <ol data-type="ordered">
+            <li>We ask for a monthly fee for using our service, all payments are "pay-as-you-go", you are
+                responsible for renewing (extending) your plan before it expires.</li>
+            <li>We take all reasonable care to make sure that our pricing pages are correct, if we discover an
+                error, we will notify you by mail.</li>
+            <li>We may change the plan pricing at our discretion, the new price will come in effect the next time
+                that you try to renew your service with us.</li>
+            <li>The pricing on our page includes taxes and excludes the transaction fee that might get charged by
+                the payment provider.</li>
+            <li>If we notify you of a mistake by us or change our pricing, you may decide to stop using our service
+                and not renew your plan. Your plan will automatically be set back to our free offering after the
+                expiration date.</li>
+            <li>If you live inside the EU (Europian Union) there is a 14 days (2 weeks) refund period, as legally
+                required by law. For any other country, we do not offer refunds and have a strict no-refunds policy.
+            </li>
+        </ol>
+        <h2>Section 4 - How to pay for your Plan</h2>
+        <ol data-type="ordered">
+            <li>You can only pay using the payment methods provided on our checkout page, no other methods are
+                available.</li>
+            <li>Transactions made through our website will be in Euro. If you wish to pay with any other currency,
+                the exchange rate and any additional transaction charges will be controlled and applied by the
+                issuing bank and not us. We will not be liable for any additional charges incurred in respect of
+                this.</li>
+        </ol>
+        <h2>Section 5 - Your Plan</h2>
+        <ol data-type="ordered">
+            <li>We offer various plans (viewable at our website: grifpkg.com) ("Plan", "Plans"). Your level of
+                service will depend on the plan that you are signed up</li>
+            <li>We may, at our discretion, offer plans to you that are free of charge ("Free plan"), that do provide
+                the benefit of `paid for` plan, on a trial basis. We may disable or withdraw any Free plan at any
+                time, at our discretion.</li>
+            <li>You can change your plan from the dashboard located at our website when you upgrade to a higher
+                plan, our website will re-calculate the days. For example, 10 days = 10 euros, and the higher plan
+                costs 20 euros, then your current days left (10 days) will change to 5 days. This is also the case
+                for downgrades, only the other way around.</li>
+            <li>When you upgrade your plan, it is required to pay the full price of the new plan before the days
+                left on the current plan will be added up.</li>
+            <li>When you no longer want to use our `paid for` service, you can decide to not renew your plan, by
+                doing so, your plan will reset to the free plan. We will not offer refunds for already paid plans.
+            </li>
+            <li>Any functionality that we offer for any plan is provided as-is.</li>
+        </ol>
+        <h2>Section 6 - Personal information</h2>
+        <ol data-type="ordered">
+            <li>We only use and store your information when necessary for providing you with our service. Please see
+                our privacy policy (https://grifpkg.com/legal/privacy) for detailed information on how we use
+                your data.</li>
+        </ol>
+        <h2>Section 7 - Authority and applicable terms</h2>
+        <ol data-type="ordered">
+            <li>To be eligible to purchase a Plan, you must (i) have full legal capacity to enter into a contract in
+                your country of residence; and (ii) if you are an individual, be at least 18 years old; and you
+                further represent and warrant to us that you have authority to bind any business on whose behalf you
+                use our site or purchase a plan to access the Services. Where you do not meet the requirements you
+                will need to ask a person who does satisfy those requirements to purchase the Plan on your behalf
+                and enter into a contract with us.</li>
+        </ol>
+        <h2>Section 8 - Our right to vary these Terms</h2>
+        <ol data-type="ordered">
+            <li>We may make changes to these terms and/or the plans by updating this page and/or the plan
+                descriptions.</li>
+            <li>If we make changes to our terms that you do not accept, you may decide to stop using our service.
+            </li>
+            <li>You will find the last updated date below on this page.</li>
+        </ol>
+        <h2>Section 9 - Your account and use of the Services</h2>
+        <ol data-type="ordered">
+            <li>We allow you to create your website (“<strong>Your Webstore</strong>”), that contains your content.
+            </li>
+            <li>The content of Your Webstore (except for any predefined content created by us) is your sole and
+                entire responsibility and all content you create is at your own risk.</li>
+            <li>You shall not upload any content to, sell any products on, or otherwise, carry out any activity on
+                Your Webstore which infringes the intellectual property rights, or any other rights, of any third
+                party.</li>
+            <li>At all times when using the Services and operating Your Webstore, you must comply fully with the
+                terms of service of the game in question.</li>
+            <li>We offer various payment gateways for you to process payments, we are not responsible for any claims
+                or problems that you might have with these providers. It is your responsibility to contact the
+                payment gateway directly in case of problems.</li>
+            <li>Your account information must be deemed "confidential", and should not be shared with anyone, except
+                for employees of your organization that is authorized to access your account.</li>
+            <li>It is your responsibility to keep your account safe, this includes, taking reasonable measures to
+                keep unauthorized users from accessing your account.</li>
+            <li>Your account is your responsibility, any activities on your account will be your responsibility. We
+                shall not be liable to you or any third party for damage and/or loss that may have been the result
+                of any failure by you to keep your account secure.</li>
+            <li>You may not attempt to, or interrupt the operations of our platform, this includes, using our
+                services for sending spam messages from our network.</li>
+        </ol>
+        <h2>Section 10 - Our service uptime</h2>
+        <ol data-type="ordered">
+            <li>We will make reasonable efforts to keep our platform operational based on "Best Effort". There may
+                be technical difficulties that result in temporary interruptions.</li>
+            <li>At no point shall you be entitled to a refund or compensation.</li>
+            <li>We are not responsible for issues that are outside of our control, this includes, but is not limited
+                to; Natural disasters, DDoS attacks, Issues with a network that is important for our service to
+                function.</li>
+            <li>We do not offer any SLA or guarantee for our services to be online.</li>
+        </ol>
+        <h2>Section 11 - License</h2>
+        <ol data-type="ordered">
+            <li>When you upgrade your plan to a paid-for plan, we grant you a limited, non-exclusive, revocable
+                license to use, view, access, download, or print content that we provide through our service, for
+                use by you for the sole purpose of creating and using your webstore during the plan period
+                ("License").</li>
+            <li>When we provide you with access to software that is marked "Open Source", you shall comply with the
+                applicable terms for such software.</li>
+            <li>Your license does not allow you to copy, adapt, reverse engineer, decompile or disassemble any of
+                our source code or software.</li>
+        </ol>
+        <h2>Section 12 - Liability</h2>
+        <ol data-type="ordered">
+            <li>We will under no circumstances whatsoever be liable to you for, but not limited to; Any loss of
+                profits, sales, business, or revenue; Loss or corruption of data, information or software; Loss of
+                business opportunity; Any indirect or consequential loss.</li>
+            <li>Our total liability to you shall in no circumstance exceed 100% of the total fees paid by you in 12
+                months (one year), before the date on which the act or omission giving rise to the liability
+                occurred.</li>
+            <li>You shall remain solely responsible for complying with your legal duties and obligations, including
+                (but without limitation) with respect for the content of your webstore (with the exception being,
+                content that we provide). </li>
+            <li>We shall not be liable to you or any of your customers for any loss or liability arising out of any
+                failure by you to comply with the terms of any agreement between you and any of your customers, or
+                for your failure to comply with any other legal duties or obligations relating to your use or
+                operation of Your Webstore.</li>
+            <li>We will not be responsible for ensuring that the content provided through the Services is suitable
+                for your purposes.</li>
+        </ol>
+        <h2>Section 13 - Indemnify</h2>
+        <ol data-type="ordered">
+            <li>You agree to indemnify, defend and hold us and our parent, subsidiaries, affiliates, partners,
+                officers, directors, agents, contractors, licensors, service providers, subcontractors, suppliers,
+                interns and employees, harmless from any claim or demand, including reasonable attorneys’ fees, made
+                by any third party due to or arising out of your breach of these Terms of Service or the documents
+                they incorporate by reference or your violation of any law or the rights of a third-party.</li>
+        </ol>
+        <h2>Section 14 - Confidential information</h2>
+        <ol data-type="ordered">
+            <li>Any non-personal information or material, except for financial information, sent to us will be
+                deemed not to be confidential. By sending us any non-personal information or material, you give us
+                an unrestricted, irrevocable license to use, reproduce, display, perform, modify, transmit and
+                distribute those materials or information.</li>
+        </ol>
+        <h2>Section 15 - Other sites</h2>
+        <ol data-type="ordered">
+            <li>When you visit or purchase from a website affiliated with us or linked to our site, you use the
+                other site at your own risk and that site’s terms and conditions of use and privacy policy will
+                apply. We have not examined the content of any other website, and are not responsible for the
+                actions, products, services, or content of any such other site or its related businesses.</li>
+        </ol>
+        <h2>Section 16 - Other important terms</h2>
+        <ol data-type="ordered">
+            <li>Each of the clauses of these Terms operates separately. If any court or relevant authority decides
+                that any of them are unlawful or unenforceable, the remaining clauses will remain in full force and
+                effect.</li>
+            <li>If we fail to insist that you perform any of your obligations under these Terms, or if we do not
+                enforce our rights against you, or if we delay in doing so, that will not mean that we have waived
+                our rights against you and will not mean that you do not have to comply with those obligations. If
+                we do waive a default by you, we will only do so in writing, and that will not mean that we will
+                automatically waive any later default by you.</li>
+            <li>These Terms of Service and any separate agreements whereby we provide you Services shall be governed
+                by and construed by the laws of Spain</li>
+            <li>These Terms of Service and any separate agreements whereby we provide you Services shall fall under
+                the exclusive jurisdiction of the courts of Spain.</li>
+            <li>Verbal or written abuse of any kind (including threats of abuse or retribution) of any grifpkg
+                customer, grifpkg employee, member, or officer will result in immediate Account termination.
+            </li>
+            <li>We do not pre-screen Materials and it is in our sole discretion to refuse or remove any Materials
+                from the Service, including your webstore.</li>
+            <li>The privacy policy found at https://grifpkg.com/legal/privacy is part of this agreement.</li>
+        </ol>
+        <h2>Section 17 - Fair Use Policy</h2>
+        <ol data-type="ordered">
+            <li>All features marked as "unlimited" fall under this FUP (Fair Use Policy)</li>
+            <li>We calculate "Fair Use" by taking the average of all other store's usage, and then adding about 20
+                of the specific item (package/category/page/etc.) on top.</li>
+            <li>We have no strict enforcing of our limits, we might contact you to tell you that your usage exceeds
+                our limits, to reduce your usage, or ask you to upgrade to a higher tier.</li>
+            <li>When you fail to comply with our requests, we have the right to remove some of the items, or limit
+                your ability to create new items.</li>
+            <li>This FUP (Fair Use Policy) is put in place to prevent abuse, some stores have created 200+ packages
+                for legitimate reasons, and are still up and running on our bronze (free) plan. We will try to be as
+                lenient as possible, but we have to act in case someone is trying to abuse our platform.</li>
+        </ol>
+    </v-sheet>
+</template>
+
+<style scoped>
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+    margin-top: 48px;
+    margin-bottom: 12px;
+}
+
+.tall-line {
+    line-height: 32px;
+}
+</style>
