ab53543248235424af9b275855aa72aeb33d0f41
quiquelhappy <quiquelhappy@gmail.com>
2023-01-02 13:50:04 +0100

    move to update

--- src/components/ComingSoon.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <v-btn target="_blank" href="https://www.buymeacoffee.com/happy" variant="text" append-icon="mdi-progress-wrench">
-        Coming Soon
-        <v-tooltip max-width="200px" activator="parent" theme="light">
-            Help grifpkg develop new tools for cool people like you!
-        </v-tooltip>
-    </v-btn>
-</template>
