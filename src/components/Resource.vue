789d4b84eb42e2895f60de3a075c6ad842e83f3e
quiquelhappy <quiquelhappy@gmail.com>
2023-01-29 17:54:14 +0100

    search

--- src/components/Resource.vue
+++ src/components/Resource.vue
@@ -2,6 +2,24 @@
   <v-card class="d-flex" style="overflow: visible">
     <div style="flex: 1 1 100%" class="pa-4">
       <v-row no-gutters align="center">
+        <v-col class="pr-7" cols="auto">
+          <v-badge color="transparent" location="bottom end" offset-x="10" offset-y="10">
+            <template v-slot:badge>
+              <v-avatar variant="flat" size="32px">
+                <v-img v-if="authors[0]?.pfp" :src="authors[0].pfp" />
+                <v-icon v-else>mdi-account</v-icon>
+                <v-tooltip theme="light" location="bottom" activator="parent">
+                  {{ authors.map(a => a.username).join(', ') }}
+                </v-tooltip>
+              </v-avatar>
+            </template>
+            <v-avatar size="54px">
+              <v-img v-if="icon" alt="Avatar" :src="icon" />
+              <v-icon v-else>mdi-package</v-icon>
+            </v-avatar>
+          </v-badge>
+
+        </v-col>
         <v-col>
           <v-list-item class="pa-0" lines="two">
             <v-list-item-title>
@@ -16,37 +34,48 @@
           </v-list-item>
         </v-col>
         <v-col cols="auto">
-          <v-chip class="mx-1" variant="text" color="primary" label size="small">
-            spigotmc.org
+          <v-chip :disabled="rating.average == null" class="mx-1" color="primary" prepend-icon="mdi-star" label
+            size="small">
+            <template v-if="rating.average != null">
+              {{ Math.round(rating.average * 5 * 10) / 10 }}/5
+            </template>
+            <template v-else>
+              ?
+            </template>
             <v-tooltip theme="light" location="bottom" activator="parent">
-              category: misc
+              <template v-if="rating.average != null">
+                {{ rating.count }} rating<template v-if="rating.count > 1">s</template>
+              </template>
+              <template v-else>
+                {{ bucket.host }} does not provide rating information
+              </template>
             </v-tooltip>
           </v-chip>
         </v-col>
         <v-col cols="auto">
           <v-chip class="mx-1" color="primary" prepend-icon="mdi-download" label size="small">
-            {{ this.numAbbr.abbreviate(downloads, 0) }}
+            {{ String(this.numAbbr.abbreviate(downloads, 0)).toUpperCase() }}
             <v-tooltip theme="light" location="bottom" activator="parent">
-              0 installs
+              {{ installs }} installs
             </v-tooltip>
           </v-chip>
         </v-col>
         <v-col cols="auto">
-          <v-chip class="mx-1" color="primary" prepend-icon="mdi-star" label size="small">
-            {{ Math.round(rating * 5 * 10) / 10 }}/5
+          <v-chip class="mx-1" variant="text" color="primary" label size="small">
+            {{ bucket.host }}
             <v-tooltip theme="light" location="bottom" activator="parent">
-              30 ratings
+              categories: {{ categories?.map(c => c.name).join(', ') }}
             </v-tooltip>
           </v-chip>
         </v-col>
-        <v-col cols="auto">
+        <!--<v-col cols="auto">
           <v-chip class="mx-1" color="primary" prepend-icon="mdi-clock" label size="small">
             DD/MM/YY
             <v-tooltip theme="light" location="bottom" activator="parent">
               latest update
             </v-tooltip>
           </v-chip>
-        </v-col>
+        </v-col>-->
       </v-row>
     </div>
     <v-sheet color="primary" style="flex: 0 0 1" class="d-flex align-center">
@@ -65,6 +94,6 @@ export default {
   data: () => ({
     numAbbr: new NumAbbr()
   }),
-  props: ["verified", "name", "description", "rating", "downloads"],
+  props: ["verified", "name", "description", "rating", "downloads", "bucket", "installs", "categories", "icon", "authors"],
 };
 </script>
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,19 +1,17 @@
 <template>
+  <v-progress-linear v-if="time == null" indeterminate></v-progress-linear>
   <v-container>
-    <v-row no-gutters>
-      <v-col
-        class="mb-3"
-        cols="12"
-        v-for="resource in resources"
-        :key="resource.name"
-      >
-        <resource
-          :verified="Math.random() < 0.3"
-          :rating="Math.random()"
-          :name="resource.name"
-          :description="resource.description"
-          :downloads="Math.floor(Math.random()*100)"
-        />
+    <v-row v-if="resources" no-gutters>
+      <v-col class="mb-3" cols="12" v-for="resource in resources" :key="resource.name">
+        <resource :verified="Math.random() < 0.3" :name="resource.name" :description="resource.description"
+          :downloads="resource.downloads" :installs="resource.installs" :rating="resource.rating"
+          :bucket="resource.bucket" :authors="resource.authors" :icon="resource.icon"
+          :categories="resource.categories" />
+      </v-col>
+      <v-col class="text-center">
+        <v-chip>
+          {{ resources.length }} resources found in {{ time }}ms
+        </v-chip>
       </v-col>
     </v-row>
   </v-container>
@@ -26,33 +24,27 @@ export default {
     Resource,
   },
   data: () => ({
-    resources: [
-      {
-        name: "SkinsRestorer",
-        description:
-          "Ability to restore/change skins on servers! (Offline and Online Mode)(Bungee/Spigot/Sponge/Velocity)",
-      },
-      {
-        name: "EssentialsX",
-        description:
-          "The modern Essentials suite for Spigot and Paper.",
-      },
-      {
-        name: "LuckPerms",
-        description:
-          "A permissions plugin for Minecraft servers (Bukkit/Spigot, BungeeCord & more)",
-      },
-      {
-        name: "ViaVersion",
-        description:
-          "Allow newer versions to connect, why lose players after a Minecraft update?",
-      },
-      {
-        name: "Vault",
-        description:
-          "Vault is a Permissions, Chat, & Economy API to give plugins easy hooks into these systems.",
-      },
-    ],
+    resources: null,
+    time: null,
   }),
+  methods: {
+    async search() {
+      this.time = null
+      const start = new Date()
+      const query = this.$route.query?.q
+      this.resources = await (await fetch('http://localhost:3000/search?' + new URLSearchParams({
+        slug: query,
+      }))).json()
+      this.time = new Date().getTime() - start.getTime()
+    }
+  },
+  watch: {
+    $route() {
+      this.search()
+    }
+  },
+  mounted() {
+    this.search()
+  }
 };
 </script>
